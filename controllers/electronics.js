e1d79d6511cbcd98d698b3f1e88e17a419589e44
Rajesh Arigela <143137417+ArigelaRajesh@users.noreply.github.com>
2023-11-08 23:59:19 -0600

    Added collection to display as list

--- controllers/electronics.js
+++ controllers/electronics.js
@@ -10,14 +10,42 @@ exports.electronics_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+// VIEWS
+// Handle a show all view
+exports.electronics_view_all_Page = async function(req, res) {
+    try{
+    electronics = await electronics.find();
+    res.render('electronics', { title: 'Electronics Search Results', results: electronics });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };   
 // for a specific electronics.
 exports.electronics_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: electronics detail: ' + req.params.id);
 };
 // Handle electronics create on POST.
-exports.electronics_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: electronics create POST');
-};
+exports.electronics_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new electronics();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"item_name":"Speaker", "brand":REdmi, "price":"150"}
+    document.item_name = req.body.item_name;
+    document.brand = req.body.brand;
+    document.price = req.body.price;
+    try{
+        let result = await document.save();
+        res.send(result);
+        }
+        catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+        }
+       };
 // Handle electronics delete form on DELETE.
 exports.electronics_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: electronics delete DELETE ' + req.params.id);
--- routes/electronics.js
+++ routes/electronics.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const electronics_controlers= require('../controllers/electronics');
 var router = express.Router();
-
-/* GET home page. */
+/* GET costumes */
 router.get('/', function(req, res, next) {
   res.render('electronics', { title: 'Search Results Electronics' });
+  router.get('/', electronics_controlers.electronics_view_all_Page );
 });
 
 module.exports = router;
