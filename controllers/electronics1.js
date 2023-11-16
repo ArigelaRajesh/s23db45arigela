072607bb18349103d5ed4b096c80e833cc232d5c
Rajesh Arigela <143137417+ArigelaRajesh@users.noreply.github.com>
2023-11-15 20:05:28 -0600

    Updated files in routes & views folders for updating  a instance in my collection

--- controllers/electronics1.js
+++ controllers/electronics1.js
@@ -26,3 +26,16 @@ exports.electronics_create_Page = function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+// Handle building the view for updating a costume.
+// query provides the id
+exports.electronics_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await electronics.findById(req.query.id)
+    res.render('electronicsupdate', { title: 'Electronics Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/electronics1.js
+++ routes/electronics1.js
@@ -6,6 +6,8 @@ var router = express.Router();
 router.get('/detail', electronics_controlers.electronics_view_one_Page);
 /* GET create electronics page */
 router.get('/create', electronics_controlers.electronics_create_Page);
+/* GET create update page */
+router.get('/update', electronics_controlers.electronics_update_Page);
 
 
 module.exports = router;
